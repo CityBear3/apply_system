0a7416c789a8dd8bbee38915baf0d51064c391d4
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-07-08 21:13:52 +0900

    [refactor]db初期化用のコードを削除した

--- src/main/kotlin/com/account_manager/apply_system/ApplySystemApplication.kt
+++ src/main/kotlin/com/account_manager/apply_system/ApplySystemApplication.kt
@@ -1,25 +1,11 @@
 package com.account_manager.apply_system
 
-import com.account_manager.apply_system.model.RoleType
-import com.account_manager.apply_system.model.UserTable
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import javax.management.relation.Role
 
 @SpringBootApplication
 class ApplySystemApplication
 
 fun main(args: Array<String>) {
-
-	CreateConnection().connection()
-	transaction {
-		addLogger(StdOutSqlLogger)
-		val record = UserTable.select { UserTable.id eq "ex1234" }.single()
-		println(record[UserTable.name])
-	}
-
 	runApplication<ApplySystemApplication>(*args)
 }
