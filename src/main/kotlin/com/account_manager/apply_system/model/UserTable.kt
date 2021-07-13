54866ea551feb685a502d8a02167a534e41f1cd7
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-07-13 19:04:16 +0900

    [feat]NotNullアノテーションを付与

--- src/main/kotlin/com/account_manager/apply_system/model/UserTable.kt
+++ src/main/kotlin/com/account_manager/apply_system/model/UserTable.kt
@@ -1,10 +1,15 @@
 package com.account_manager.apply_system.model
 
+import org.jetbrains.annotations.NotNull
 import org.jetbrains.exposed.sql.Table
 
 object UserTable: Table("users") {
+    @NotNull
     val id = varchar("id", 16)
+    @NotNull
     val name = varchar("name", 32)
+    @NotNull
     val password = varchar("password", 64)
+    @NotNull
     val role = varchar("role", 6)
 }
