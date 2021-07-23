72914362bd42c47fe8be989b39b546255f59b667
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-07-23 20:01:02 +0900

    [add]購入申請を格納するテーブルのスキーマ

---
+++ src/main/resources/db/migration/V1.1.0__Create-ApplyRequestTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE request (
+    id int NOT NULL AUTO_INCREMENT,
+    userid varchar(16) NOT NULL,
+    product varchar(256) NOT NULL,
+    purpose varchar(32) NOT NULL,
+    reason varchar(256) NOT NULL,
+    PRIMARY KEY(id)
+);
