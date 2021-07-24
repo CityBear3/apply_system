c0a07b2c5d506468ef4f19d799b8a2f42395017b
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-07-25 05:09:09 +0900

    [feat]mockitoを依存関係に追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 	testImplementation("org.assertj:assertj-core:3.19.0")
+	testImplementation("org.mockito:mockito-core:3.8.0")
+	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 }
 
 tasks.withType<KotlinCompile> {
