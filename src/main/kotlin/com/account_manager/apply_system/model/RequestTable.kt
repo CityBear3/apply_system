55ba601d66c4e66bb8f8d51d6e6c4519bf410036
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-07-26 15:26:09 +0900

    [add]購入申請テーブルのオブジェクト

---
+++ src/main/kotlin/com/account_manager/apply_system/model/RequestTable.kt
@@ -0,0 +1,17 @@
+package com.account_manager.apply_system.model
+
+
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.exposed.sql.Table
+
+object RequestTable : Table("request") {
+    val id = integer("id").autoIncrement()
+    @NotNull
+    val userID = varchar("userid", 16)
+    @NotNull
+    val product = varchar("product", 256)
+    @NotNull
+    val propose = varchar("propose", 32)
+    @NotNull
+    val reason = varchar("reason", 256)
+}
