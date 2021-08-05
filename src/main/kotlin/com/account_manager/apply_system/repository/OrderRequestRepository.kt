48896549c2132a0ee8103cacea0f8136731ef4f3
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-08-05 19:47:02 +0900

    [refactor]軽微な修正

--- src/main/kotlin/com/account_manager/apply_system/repository/OrderRequestRepository.kt
+++ src/main/kotlin/com/account_manager/apply_system/repository/OrderRequestRepository.kt
@@ -2,7 +2,6 @@ package com.account_manager.apply_system.repository
 
 import com.account_manager.apply_system.model.OrderInfoModel
 
-
 interface OrderRequestRepository {
     fun receiveRequest(orderInfoModel: OrderInfoModel): Boolean
 }
