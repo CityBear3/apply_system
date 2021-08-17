3f833c4947b84097c35b3d1196de6c324f8dd5ea
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-08-17 21:35:15 +0900

    [feat]Spring Aopを依存関係に追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("org.springframework.session:spring-session-data-redis")
 	implementation("redis.clients:jedis")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
